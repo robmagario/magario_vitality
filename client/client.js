3637576f4705f5861cf886e8f975266b98e59faf
chinho <chinho.ng@magario.com>
2015-04-22 17:57:39 -0700

    let the button on top menu can scroll to the tag. leave the main frame will have a background on top menu.

--- client/client.js
+++ client/client.js
@@ -4,6 +4,20 @@
 this.App = {};
 this.Helpers = {};
 
+Helpers.Veriables = {
+    ScrollSpeed: 1000 // Scroll by 1 second
+};
+
+// User for show log while testing
+Helpers.Log = {
+    active: true,
+    Show: function(header, message) {
+        if(Helpers.Log.active) {
+            console.log("[" + header + "]: " + message);
+        }
+    }
+};
+
 Helpers.helloworld = function() {
     return "Hello World";
 };
---
+++ client/view/agency/agency.js
@@ -0,0 +1,61 @@
+/**
+ * Created by user on 4/22/2015.
+ */
+Template.Agency.rendered = function() {
+    /*TEMPLATE_RENDERED_CODE*/
+};
+
+Template.Agency.events({
+    'click a.page-scroll': function(event) {
+        Helpers.Log.Show("Click", "a.page-scroll");
+        var tagname = event.target.innerText.toLowerCase();
+        var targetY = 0;
+        if(tagname != null && tagname != "") {
+            targetY = $('#'+tagname).offset().top;
+        } else if(event.target.localName == "i") {
+            targetY = $('#about').offset().top;
+        }
+        $('body').animate({scrollTop:targetY}, Helpers.Veriables.ScrollSpeed);
+    },
+
+    'mouseenter header': function() {
+        $('.navbar-inverse').addClass('navbar-expanded');
+    },
+    'mouseleave header': function() {
+        $('.navbar-inverse').removeClass('navbar-expanded');
+    },
+    'mouseenter #about': function() {
+        $('.focus-about').addClass('active');
+    },
+    'mouseleave #about': function() {
+        $('.focus-about').removeClass('active');
+    },
+    'mouseenter #process': function() {
+        $('.focus-process').addClass('active');
+    },
+    'mouseleave #process': function() {
+        $('.focus-process').removeClass('active');
+    },
+    'mouseenter #work': function() {
+        $('.focus-work').addClass('active');
+    },
+    'mouseleave #work': function() {
+        $('.focus-work').removeClass('active');
+    },
+    'mouseenter #pricing': function() {
+        $('.focus-pricing').addClass('active');
+    },
+    'mouseleave #pricing': function() {
+        $('.focus-pricing').removeClass('active');
+    },
+    'mouseenter #contact': function() {
+        $('.focus-contact').addClass('active');
+    },
+    'mouseleave #contact': function() {
+        $('.focus-contact').removeClass('active');
+    }
+});
+
+Template.Agency.helpers({
+
+});
